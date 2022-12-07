d4569009579650fe74817c883472571a6dfbe869
Guilheme Luis Franca <guilhermeluisfranca404@outlook.com>
2022-12-06 21:36:48 -0300

    basicao da pagina inicial

---
+++ src/app/base/global/modules/modules.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ButtonModule,
+  ],
+  exports: [
+    ButtonModule,
+  ]
+})
+export class ModulesModule { }
