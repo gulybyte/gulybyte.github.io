c764a3e720b6d68aad094ae99553fa04d815357a
Guilheme Luis Franca <guilhermeluisfranca404@outlook.com>
2022-12-09 21:41:23 -0300

    adicionado header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModulesModule } from './base/global/modules/modules.module';
+import { ComponentsHomeComponent } from './components-home/components-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsHomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components-home/components-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components-home',
+  templateUrl: './components-home.component.html',
+  styleUrls: ['./components-home.component.css']
+})
+export class ComponentsHomeComponent {
+
+}
