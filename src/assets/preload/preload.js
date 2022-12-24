01956066df703f9c741038fd81ffe3de773440f2
Guilheme Luis Franca <guilhermeluisfranca404@outlook.com>
2022-12-24 16:19:17 -0300

    preloading page

---
+++ src/assets/preload/preload.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+  setTimeout(function(){
+    document.getElementById("preloader").style.display = "none";
+    document.body.style.overflow = "scroll";
+  }, 800);
+});
