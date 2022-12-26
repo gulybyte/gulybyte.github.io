7f84c1d90cc2eaabe6f284c03290adcc069b9711
Guilheme Luis Franca <guilhermeluisfranca404@outlook.com>
2022-12-26 02:37:23 -0300

    design responsive about-me + probleminha de reponsividade nav-menu, (responsivade completa)

--- src/app/components/about/components-about.component.ts
+++ src/app/components/about/components-about.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-components-about',
   templateUrl: './components-about.component.html',
-  styleUrls: ['./components-about.component.css']
+  styleUrls: [
+    './components-about.component.css',
+    './components-about-1048.component.css'
+  ]
 })
 export class ComponentsAboutComponent {
 
