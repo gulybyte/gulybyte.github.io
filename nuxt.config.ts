c4e47e04d23123838642f1bbc93c88bcd0e1d6d4
Guilherme Luis <gulybyte@gmail.com>
2023-04-19 17:48:41 -0300

    primeicons e primevue adicionado

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,15 @@ export default defineNuxtConfig({
   css: [
     '~/assets/_colors.scss',
     '~/assets/_fonts.scss',
-    '~/assets/_base.scss'
+    '~/assets/_base.scss',
+
+    "primeicons/primeicons.css",
+    "primevue/resources/themes/lara-dark-teal/theme.css",
+    "primevue/resources/primevue.min.css"
   ],
+  build: {
+    transpile: ["primevue"]
+  },
   app: {
     head: {
       charset: 'utf-16',
--- pages/gulybyte.vue
+++ pages/gulybyte.vue
@@ -1,4 +1,53 @@
 <template>
-  <HeadGulyByte></HeadGulyByte>
-  <img src="/static/images/eu.webp" alt="Discover Nuxt 3" />
+  <gulybyte-Head></gulybyte-Head>
+  <img src="/static/images/eu.webp" alt="Guilherme Luis França" />
+  <Button label="Check" icon="pi pi-check" />
+
+  <!-- <app-components-base></app-components-base> -->
+
+  <div class="container">
+
+    <!-- <app-components-infoinit></app-components-infoinit> -->
+
+    <div class="clear"></div>
+
+    <!-- <app-components-about></app-components-about> -->
+
+    <div class="clear"></div>
+
+    <!-- <app-component-philosophy></app-component-philosophy> -->
+
+    <div class="clear"></div>
+
+
+    <div class="soon-more" style="margin: 80px; text-align: center; font-size: 25px; font-family: 'Calibre-semibold';">
+      <i class="pi pi-exclamation-triangle" style="color: yellow; font-size: 25px;"></i> Ainda em construção! <i
+        style="font-size: 25px;" class="pi pi-spin pi-spinner"></i> Em breve mais conteudo <i
+        style="font-size: 25px;" class="pi pi-calendar-plus"></i>
+    </div>
+  </div>
+
+
+
 </template>
+
+
+<style lang="scss">
+  @media screen and (max-width: 419px) {
+    .soon-more {
+      font-size: 22px !important;
+      i {
+        font-size: 22px !important;
+      }
+    }
+  }
+
+  @media screen and (max-width: 380px) {
+    .soon-more {
+      font-size: 18px !important;
+      i {
+        font-size: 18px !important;
+      }
+    }
+  }
+</style>
