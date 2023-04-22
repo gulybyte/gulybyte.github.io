44ca691b0c1a2aee6839339d76eb5fb01751946c
Guilherme Luis <gulybyte@gmail.com>
2023-04-22 00:37:26 -0300

    icone g otimizado

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     "primevue/resources/primevue.min.css"
   ],
   build: {
-    transpile: ["primevue"]
+    transpile: ["primevue", "aos"],
   },
   modules: [
     '@nuxtjs/tailwindcss',
