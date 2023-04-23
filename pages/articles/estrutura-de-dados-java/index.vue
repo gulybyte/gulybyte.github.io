3d156883ab9ddb1aa6974f4f34dcb3341f6fd215
Guilherme Luis <gulybyte@gmail.com>
2023-04-23 07:02:12 -0300

    hierarquia

--- pages/articles/estrutura-de-dados-java/index.vue
+++ pages/articles/estrutura-de-dados-java/index.vue
@@ -8,10 +8,10 @@
       alt="Dados Estruturados x Não Estruturados" />
 
     <nuxt-img src="/static/images/linha-branca.png"
-      alt="Dados Estruturados x Não Estruturados" />
+      alt="Linha Branca" />
 
     <section>
-      <h3 class="text-3xl">Porque estudar?</h3>
+      <h3>Porque estudar?</h3>
 
       <p>&nbsp;&nbsp;A disciplina de estrutura de dados visa estudar como os dados são organizados,
         acessados e manipulados na memória. Em linguagens de alto nível, geralmente usamos as
@@ -27,7 +27,7 @@
     </section>
 
     <section>
-      <h3 class="text-3xl">Compreendendo Generics do Java.</h3>
+      <h3>Compreendendo Generics do Java.</h3>
 
       <p>&nbsp;&nbsp;Antes de partir para as estruturas, é necessário compreender os Generics do Java.
         Generics não apenas tornam o código mais seguro, mas também mais legível.</p>
@@ -41,9 +41,23 @@
     </section>
 
     <section>
-      <h3 class="text-3xl">Hierarquia Collection Framework.</h3>
+      <h3>Hierarquia Collection Framework.</h3>
 
+      <p>&nbsp;&nbsp;O Collection Framework do Java é um conjunto de classes e interfaces que fornecem estruturas de dados
+        de alto nível e eficiente para armazenar e manipular objetos em Java. É onde fica todas as estruturas de dados
+        padrão da linguagem.</p>
 
+      <p>&nbsp;&nbsp;As <b>principais interfaces e classes</b> que vamos estudar são essas
+        (<a href="https://docs.oracle.com/en/java/javase/17/docs/api/overview-tree.html">mas
+          é só o básico e o mais usado, atualmente essa lista é muito mas muito mais grande</a>):</p>
+
+      <nuxt-img class="m-auto w-11/12" src="/static/images/articles/estrutura-de-dados-java/collections-framework-java.png"
+        alt="Dados Estruturados x Não Estruturados" />
+
+      <p>&nbsp;&nbsp;Lembrando que essa hierarquia são só as principais, e inclusive ela está bem rudimentar, faltou colocar as
+        "Abstract's" interface e todas as outras estruturas fornecidas pela Collections Framework.</p>
+
+      <p>&nbsp;&nbsp;Breve história...</p>
 
     </section>
 
@@ -68,6 +82,7 @@
   }
 
   h3 {
+    font-size: 35px;
     padding-top: 80px;
   }
 
