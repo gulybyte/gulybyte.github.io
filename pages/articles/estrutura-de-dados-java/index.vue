a82d339249c81c0abbc5d2c43f51dcc65b4fe0c9
Guilherme Luis <gulybyte@gmail.com>
2023-04-28 22:43:28 -0300

    head meta tags para artigo estrutura de dados

--- pages/articles/estrutura-de-dados-java/index.vue
+++ pages/articles/estrutura-de-dados-java/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <HeadGeneric></HeadGeneric> -->
+  <HeadGeneric></HeadGeneric>
 
   <main class="articles-tecnico-container">
 
@@ -132,11 +132,34 @@
 }
 </style>
 
-<script setup lang="ts">
+<script setup>
 useHead({
-  title: 'My App',
+  title: 'Estrutura de Dados',
+  link: [
+    { rel: 'canonical', href: 'https://gulybyte.github.io/articles/estrutura-de-dados-java' }
+  ],
   meta: [
-    { name: 'description', content: 'My amazing site.' }
+    { property: 'og:title', content: 'Estrutura de Dados.' },
+
+    { name: 'description', content: 'Serie - visão geral sobre estrutura de dados Java.'},
+    { name: 'excerpt', content: 'Serie - visão geral sobre estrutura de dados Java.'},
+    { name: 'og:description', content: 'Serie - visão geral sobre estrutura de dados Java.'},
+
+    { name: 'keywords', content: 'Java, Estrutura de Dados, Série, List, Listas, Stack, Pilha, Árvores, Trees, Filas, Queues, Mapas, Maps, Set, Algoritmos e Estrutura de Dados Java'},
+
+    { name: 'application-name', content: 'Serie - visão geral sobre estrutura de dados Java.'},
+    { name: 'og:site_name', content: 'Serie - visão geral sobre estrutura de dados Java.'},
+
+    { name: 'og:image', content: 'https://gulybyte.github.io/static/images/articles/estrutura-de-dados-java/dados-estruturados-x-nao-estruturados.png'},
+    { name: 'og:image:width', content: '712'},
+    { name: 'og:image:height', content: '351'},
+    { name: 'og:image:type', content: 'image/png'},
+
+    { name: 'og:url', content: 'https://gulybyte.github.io/articles/estrutura-de-dados-java'},
+
+    { name: 'article:published_time', content: '2023-03-25T04:09:45+00:00'},
+    { name: 'article:modified_time', content: '2023-04-30T04:30:41+00:00'}
+
   ]
 })
 </script>
