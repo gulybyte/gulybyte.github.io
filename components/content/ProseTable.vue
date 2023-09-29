c15f9eb30cd727f9c7c7eea6873210f351eb7287
gulybyte <gulybyte@gmail.com>
2023-09-29 15:52:58 -0300

    correção tabela passando tamanho dos markdowns na tela movel

--- components/content/ProseTable.vue
+++ components/content/ProseTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <table role="table" class="p-datatable-table" style="min-width: 100%; margin: 0 auto 1em;">
+  <table role="table" class="p-datatable-table" style="max-width: 100%; margin: 0 auto 1em;">
     <slot />
   </table>
 </template>
